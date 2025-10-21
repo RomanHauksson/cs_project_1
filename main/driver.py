ad515029fd04f3c1ff3b8fd99574cbdd22be7e1e
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2025-10-20 23:13:25 -0500

    Add more options to driver

--- main/driver.py
+++ main/driver.py
@@ -5,20 +5,66 @@ from subprocess import PIPE, Popen
 
 from simple_term_menu import TerminalMenu
 
-log_filename = sys.argv[1]
 
-logger = Popen(
-    ["python3.13", "logger.py", log_filename], stdout=PIPE, stdin=PIPE, encoding="utf8"
-)
+def main():
+    log_filename = sys.argv[1]
 
-encryption = Popen(
-    ["python3.13", "encryption.py"], stdout=PIPE, stdin=PIPE, encoding="utf8"
-)
+    logger = Popen(
+        ["python3.13", "logger.py", log_filename],
+        stdout=PIPE,
+        stdin=PIPE,
+        encoding="utf8",
+    )
 
-logger.stdin.write("START")
+    encryption = Popen(
+        ["python3.13", "encryption.py"], stdout=PIPE, stdin=PIPE, encoding="utf8"
+    )
 
-while True:
-    print("Menu")
+    logger.stdin.write("START")
+
+    options_main = ["password", "encrypt", "decrypt", "history", "quit"]
+    menu_main = TerminalMenu(options_main)
+    string_history = []
+    while True:
+        option_main = menu_main.show()
+        match options_main[option_main]:
+            case "password":
+                options_password = [
+                    "Set the password to a string from your history",
+                    "Set the password to a new string",
+                ]
+                option_password = TerminalMenu(options_password).show()
+                match options_password[option_password]:
+                    case "Set the password to a string from your history":
+                        option_password_history = TerminalMenu(string_history).show()
+                        password = string_history[option_password_history]
+                    case "Set the password to a new string":
+                        print("Enter a string: ")
+                        password = sys.stdin.readline().rstrip()
+                encryption.stdin.write(f"PASS {password}")
+            case "encrypt":
+                options_encrypt = [
+                    "Encrypt a string from your history",
+                    "Encrypt a new string",
+                ]
+                option_encrypt = TerminalMenu(options_encrypt).show()
+                match options_encrypt[option_encrypt]:
+                    case "Encrypt a string from your history":
+                        option_encrypt_history = TerminalMenu(string_history).show()
+                        string = string_history[option_encrypt_history]
+                    case "Encrypt a new string":
+                        print("Enter a string: ")
+                        string = sys.stdin.readline().rstrip()
+                        string_history.append(string)
+                encryption.stdin.write(f"ENCRYPT {password}")
+                encrypted_string = encryption.stdout.read()
+                string_history.append(encrypted_string)
+                print(encrypted_string)
+            case "decrypt":
+              
+
+    logger.stdin.write("EXIT")
 
 
-logger.stdin.write("EXIT")
+if __name__ == "__main__":
+    main()
