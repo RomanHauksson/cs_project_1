f5391ef0294328a9302868347b45d4039b5c6710
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2025-10-20 23:36:05 -0500

    Handle quitting logger and logs without a message

--- main/logger.py
+++ main/logger.py
@@ -11,9 +11,10 @@ while True:
     line = sys.stdin.readline().rstrip().split(" ", 1)
 
     action = line[0]
-    message = line[1]
     if action == "QUIT":
         break
+
+    message = "" if len(line) == 1 else line[1]
 
     with open(filename, "a") as f:
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M")
