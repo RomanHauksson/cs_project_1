b12b2c229b344d47944c46374bb5494d9ac16e35
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2025-10-21 20:41:16 -0500

    Break out two functions + don't add password to history

--- main/driver.py
+++ main/driver.py
@@ -26,119 +26,97 @@ def main():
     options_main = ["password", "encrypt", "decrypt", "history", "quit"]
     menu_main = TerminalMenu(options_main)
     string_history = []
+
+    def log(message: str):
+        logger.stdin.write(message)
+        logger.stdin.flush()
+
+    def get_string(
+        history_message: str, new_string_message: str, add_to_history: bool = True
+    ):
+        selected_option = TerminalMenu([history_message, new_string_message]).show()
+        if selected_option == 0:
+            if len(string_history) == 0:
+                print("No strings in history")
+                return None
+            history_index = TerminalMenu(string_history).show()
+            return string_history[history_index]
+        else:
+            print("Enter a string: ")
+            string = sys.stdin.readline().rstrip()
+            if not string.isalpha():
+                raise ValueError("Input must contain only letters.")
+            if add_to_history:
+                string_history.append(string)
+            return string
+
     while True:
         option_main = menu_main.show()
         match options_main[option_main]:
             case "password":
-                option_password = TerminalMenu(
-                    [
-                        "Set the password to a string from your history",
-                        "Set the password to a new string",
-                    ]
-                ).show()
-                match option_password:
-                    case 0:
-                        if len(string_history) == 0:
-                            print("No strings in history")
-                            continue
-                        option_password_history = TerminalMenu(string_history).show()
-                        password = string_history[option_password_history]
-                    case 1:
-                        print("Enter a string: ")
-                        password = sys.stdin.readline().rstrip()
+                password = get_string(
+                    "Set the password to a string from the history",
+                    "Set the password to a new string",
+                    add_to_history=False,
+                )
+                if password is None:
+                    continue
                 encryption.stdin.write(f"PASS {password}\n")
                 encryption.stdin.flush()
-                logger.stdin.write(f"PASS Password has been set to {password}\n")
-                logger.stdin.flush()
+                log(f"PASS Password has been set to {password}\n")
+                print(f"Password has been set to {password}")
             case "encrypt":
-                option_encrypt = TerminalMenu(
-                    [
-                        "Encrypt a string from your history",
-                        "Encrypt a new string",
-                    ]
-                ).show()
-                match option_encrypt:
-                    case 0:
-                        if len(string_history) == 0:
-                            print("No strings in history")
-                            continue
-                        option_encrypt_history = TerminalMenu(string_history).show()
-                        string = string_history[option_encrypt_history]
-                    case 1:
-                        print("Enter a string: ")
-                        string = sys.stdin.readline().rstrip()
-                        string_history.append(string)
+                string = get_string(
+                    "Encrypt a string from your history",
+                    "Encrypt a new string",
+                )
+                if string is None:
+                    continue
+                log(f'ENCRYPT The string "{string}" is being encrypted\n')
                 encryption.stdin.write(f"ENCRYPT {string}\n")
                 encryption.stdin.flush()
-                logger.stdin.write(
-                    f'ENCRYPT The string "{string}" has been encrypted\n'
-                )
-                logger.stdin.flush()
                 response = encryption.stdout.readline().rstrip()
                 if response.startswith("RESULT "):
                     encrypted_string = response[7:]
                 else:
-                    encrypted_string = response
-                logger.stdin.write(
-                    f'ENCRYPT "{string}" encrypted is "{encrypted_string}"\n'
-                )
-                logger.stdin.flush()
+                    print(response)
+                    continue
+                log(f'ENCRYPT "{string}" encrypted is "{encrypted_string}"\n')
                 string_history.append(encrypted_string)
-                print("Encrypted string: " + encrypted_string)
+                print(f'"{string}" encrypted is "{encrypted_string}"')
             case "decrypt":
-                option_decrypt = TerminalMenu(
-                    [
-                        "Decrypt a string from your history",
-                        "Decrypt a new string",
-                    ]
-                ).show()
-                match option_decrypt:
-                    case 0:
-                        if len(string_history) == 0:
-                            print("No strings in history")
-                            continue
-                        option_decrypt_history = TerminalMenu(string_history).show()
-                        string = string_history[option_decrypt_history]
-                    case 1:
-                        print("Enter a string: ")
-                        string = sys.stdin.readline().rstrip()
-                        string_history.append(string)
+                string = get_string(
+                    "Decrypt a string from your history",
+                    "Decrypt a new string",
+                )
+                if string is None:
+                    continue
+                log(f'DECRYPT The string "{string}" is being decrypted\n')
                 encryption.stdin.write(f"DECRYPT {string}\n")
                 encryption.stdin.flush()
-                logger.stdin.write(
-                    f'DECRYPT The string "{string}" has been decrypted\n'
-                )
-                logger.stdin.flush()
                 response = encryption.stdout.readline().rstrip()
                 if response.startswith("RESULT "):
                     decrypted_string = response[7:]
                 else:
-                    decrypted_string = response
-                logger.stdin.write(
-                    f'DECRYPT "{string}" decrypted is "{decrypted_string}"\n'
-                )
-                logger.stdin.flush()
+                    print(response)
+                    continue
+                log(f'DECRYPT "{string}" decrypted is "{decrypted_string}"\n')
                 string_history.append(decrypted_string)
-                print(decrypted_string)
+                print(f'"{string}" decrypted is "{decrypted_string}"')
             case "history":
-                logger.stdin.write("HISTORY The user requested to see the history\n")
-                logger.stdin.flush()
+                log("HISTORY The user requested to see the history\n")
                 if len(string_history) == 0:
                     print("No strings in history")
                 else:
                     print("String history: ")
                     for string in string_history:
                         print(f"- {string}")
-                logger.stdin.write(
-                    f"HISTORY The current history is {', '.join(string_history)}\n"
-                )
-                logger.stdin.flush()
+                log(f"HISTORY The current history is {', '.join(string_history)}\n")
             case "quit":
                 print("Quitting...")
                 encryption.stdin.write("QUIT\n")
                 encryption.stdin.flush()
-                logger.stdin.write("QUIT The user quit the program\n")
-                logger.stdin.flush()
+                log("QUIT The user quit the program\n")
                 break
 
     encryption.wait()
